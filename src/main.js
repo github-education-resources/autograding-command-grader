4d65dd373b030c9e8ff7d1b4dd959d707e3f844b
Steven Nunez <octosteve@github.com>
2023-10-18 19:34:28 -0400

    Refactor Tests

--- src/main.js
+++ src/main.js
@@ -1,83 +1,72 @@
 const core = require('@actions/core');
-const {
-    execSync
-} = require('child_process');
+const { execSync } = require('child_process');
 
 function btoa(str) {
     return Buffer.from(str).toString('base64');
+}
+
+function generateResult(status, testName, command, message, duration, maxScore) {
+    return {
+        version: 1,
+        status: status,
+        max_score: maxScore,
+        tests: [{
+            name: testName,
+            status: status,
+            score: status === 'pass' ? maxScore : 0,
+            message: message,
+            test_code: command,
+            filename: "",
+            line_no: 0,
+            duration: duration
+        }]
+    };
+}
+
+function getErrorMessage(error, command) {
+    if (error.message.includes("ETIMEDOUT")) {
+        return "Command timed out";
+    }
+    if (error.message.includes("command not found")) {
+        return `Unable to locate executable file: ${command}`;
+    }
+    if (error.message.includes("Command failed")) {
+        return "failed with exit code 1";
+    }
+    return error.message;
 }
 
 function run() {
     const testName = core.getInput('test-name', { required: true });
     const setupCommand = core.getInput('setup-command');
     const command = core.getInput('command', { required: true });
-
-    const timeout = parseFloat(core.getInput('timeout')) * 60000; // Convert to ms
+    const timeout = parseFloat(core.getInput('timeout') || 60) * 60000; // Convert to minutes
     const maxScore = parseInt(core.getInput('max-score') || 0);
 
-    let myOutput = '';
+    let output = '';
     let startTime;
+    let endTime;
+    let result;
 
     try {
         if (setupCommand) {
-            execSync(setupCommand, {
-                timeout: timeout
-            });
+            execSync(setupCommand, { timeout: timeout });
         }
 
         startTime = new Date();
-        myOutput = execSync(command, {
-            timeout: timeout
-        }).toString();
+        output = execSync(command, { timeout: timeout }).toString();
+        endTime = new Date();
 
-        const endTime = new Date();
-        const result = {
-            version: 1,
-            status: 'pass',
-            max_score: maxScore,
-            tests: [{
-                name: testName,
-                status: 'pass',
-                score: maxScore,
-                message: myOutput,
-                test_code: `${command}`,
-                filename: "",
-                line_no: 0,
-                duration: endTime - startTime
-            }]
-        }
-
-        core.setOutput('result', btoa(JSON.stringify(result)));
+        result = generateResult('pass', testName, command, output, endTime - startTime, maxScore);
 
     } catch (error) {
-        let message = error.message;
-
-        if (message.includes("ETIMEDOUT")) {
-            message = "Command timed out";
-        } else if (message.includes("command not found")) {
-            message = "Unable to locate executable file: " + command;
-        } else if (message.includes("Command failed")) {
-            message = "failed with exit code 1"
-        }
-        const endTime = new Date();
-        const result = {
-            version: 1,
-            status: 'fail',
-            max_score: maxScore,
-            tests: [{
-                name: testName,
-                status: 'fail',
-                score: 0,
-                message: message,
-                test_code: `${command}`,
-                filename: "",
-                line_no: 0,
-                duration: endTime - startTime
-            }]
-        }
-
-        core.setOutput('result', btoa(JSON.stringify(result)));
+        endTime = new Date();
+        const errorMessage = getErrorMessage(error, command);
+        result = generateResult('fail', testName, command, errorMessage, endTime - startTime, maxScore);
     }
+
+    core.setOutput('result', btoa(JSON.stringify(result)));
 }
 
 run();
+
