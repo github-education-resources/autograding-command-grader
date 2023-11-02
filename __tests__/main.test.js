52b9dcb3d0bac711ef80a13b971c2e204c9e51d2
Ashley <saturn226@github.com>
2023-11-02 14:54:59 -0400

    added original env vars

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,146 +1,160 @@
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const process = require('process')
+const cp = require('child_process')
+const path = require('path')
 
-const np = process.execPath;
-const ip = path.join(__dirname, '..', 'src', 'main.js');
+const np = process.execPath
+const ip = path.join(__dirname, '..', 'src', 'main.js')
 
 function runTestWithEnv(env) {
-    const options = {
-        env: {
-            ...process.env,
-            ...env
-        },
-        encoding: 'utf-8'
-    };
-    const child = cp.spawnSync(np, [ip], options);
-    const stdout = child.stdout.toString();
-    const encodedResult = stdout.split('::set-output name=result::')[1].trim();
-    return JSON.parse(atob(encodedResult));
+  const options = {
+    env: {
+      ...process.env,
+      ...env,
+    },
+    encoding: 'utf-8',
+  }
+  const child = cp.spawnSync(np, [ip], options)
+  const stdout = child.stdout.toString()
+  const encodedResult = stdout.split('::set-output name=result::')[1].trim()
+  return JSON.parse(atob(encodedResult))
 }
 
 function atob(str) {
-    return Buffer.from(str, 'base64').toString('utf8');
+  return Buffer.from(str, 'base64').toString('utf8')
 }
 
 test('test runs', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 1',
-        'INPUT_COMMAND': 'echo Hello, World!',
-        'INPUT_TIMEOUT': '5'
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 1',
+    INPUT_COMMAND: 'echo Hello, World!',
+    INPUT_TIMEOUT: '5',
+  })
 
-    expect(result.status).toBe('pass');
-    expect(result.tests[0].name).toBe('Test 1');
-    expect(result.tests[0].status).toBe('pass');
-    expect(result.tests[0].message).toContain('Hello, World!\n');
-});
+  expect(result.status).toBe('pass')
+  expect(result.tests[0].name).toBe('Test 1')
+  expect(result.tests[0].status).toBe('pass')
+  expect(result.tests[0].message).toContain('Hello, World!\n')
+})
 
+test('contains pre-set environment variables', () => {
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 1',
+    INPUT_COMMAND: 'env',
+    'INPUT_EXPECTED-OUTPUT': 'Failing on purpose',
+    'INPUT_COMPARISON-METHOD': 'exact',
+  })
+
+  expect(result.tests[0].message).toContain(`PATH=${process.env.PATH}`)
+  expect(result.tests[0].message).toContain('FORCE_COLOR=true')
+  expect(result.tests[0].message).toContain('DOTNET_CLI_HOME=/tmp')
+  expect(result.tests[0].message).toContain('DOTNET_NOLOGO=true')
+  expect(result.tests[0].message).toContain(`HOME=${process.env.HOME}`)
+})
 
 test('awards score if provided', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 1',
-        'INPUT_COMMAND': 'echo Hello, World!',
-        'INPUT_MAX-SCORE': '100'
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 1',
+    INPUT_COMMAND: 'echo Hello, World!',
+    'INPUT_MAX-SCORE': '100',
+  })
 
-    expect(result.max_score).toBe(100);
-    expect(result.tests[0].score).toBe(100);
-});
+  expect(result.max_score).toBe(100)
+  expect(result.tests[0].score).toBe(100)
+})
 
 test('falls back to 0 points if none provided', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 1',
-        'INPUT_COMMAND': 'echo Hello, World!',
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 1',
+    INPUT_COMMAND: 'echo Hello, World!',
+  })
 
-    expect(result.max_score).toBe(0);
-    expect(result.tests[0].score).toBe(0);
-});
+  expect(result.max_score).toBe(0)
+  expect(result.tests[0].score).toBe(0)
+})
 
 test('test fails on bad logic', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 2',
-        'INPUT_COMMAND': 'node -e "process.exit(1);"',
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 2',
+    INPUT_COMMAND: 'node -e "process.exit(1);"',
+  })
 
-    expect(result.status).toBe('fail');
-    expect(result.tests[0].name).toBe('Test 2');
-    expect(result.tests[0].status).toBe('fail');
-    expect(result.tests[0].message).toContain('failed with exit code 1');
-});
+  expect(result.status).toBe('fail')
+  expect(result.tests[0].name).toBe('Test 2')
+  expect(result.tests[0].status).toBe('fail')
+  expect(result.tests[0].message).toContain('failed with exit code 1')
+})
 
 test('awards no points if test fails', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 2',
-        'INPUT_COMMAND': 'node -e "process.exit(1);"',
-        'INPUT_MAX-SCORE': '100'
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 2',
+    INPUT_COMMAND: 'node -e "process.exit(1);"',
+    'INPUT_MAX-SCORE': '100',
+  })
 
-    expect(result.max_score).toBe(100);
-    expect(result.tests[0].score).toBe(0);
-});
+  expect(result.max_score).toBe(100)
+  expect(result.tests[0].score).toBe(0)
+})
 
 test('test fails on bad code', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 3',
-        'INPUT_COMMAND': 'node -e "console.log(a);"',
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 3',
+    INPUT_COMMAND: 'node -e "console.log(a);"',
+  })
 
-    // Asserting on specific properties of the result
-    expect(result.status).toBe('fail');
-    expect(result.tests[0].name).toBe('Test 3');
-    expect(result.tests[0].status).toBe('fail');
-    expect(result.tests[0].message).toContain('failed with exit code 1');
-});
+  // Asserting on specific properties of the result
+  expect(result.status).toBe('fail')
+  expect(result.tests[0].name).toBe('Test 3')
+  expect(result.tests[0].status).toBe('fail')
+  expect(result.tests[0].message).toContain('failed with exit code 1')
+})
 
 test('test fails on non-existent executable', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Test 4',
-        'INPUT_COMMAND': 'nonexistentcommand',
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Test 4',
+    INPUT_COMMAND: 'nonexistentcommand',
+  })
 
-    expect(result.status).toBe('fail');
-    expect(result.tests[0].name).toBe('Test 4');
-    expect(result.tests[0].status).toBe('fail');
-    expect(result.tests[0].message).toContain('Unable to locate executable file: nonexistentcommand');
-});
+  expect(result.status).toBe('fail')
+  expect(result.tests[0].name).toBe('Test 4')
+  expect(result.tests[0].status).toBe('fail')
+  expect(result.tests[0].message).toContain('Unable to locate executable file: nonexistentcommand')
+})
 
 test('test fails on command timeout', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Timeout Test',
-        'INPUT_COMMAND': 'sleep 3',
-        'INPUT_TIMEOUT': '0.01' // ~ 1 second
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Timeout Test',
+    INPUT_COMMAND: 'sleep 3',
+    INPUT_TIMEOUT: '0.01', // ~ 1 second
+  })
 
-    expect(result.status).toBe('fail');
-    expect(result.tests[0].name).toBe('Timeout Test');
-    expect(result.tests[0].status).toBe('fail');
-    expect(result.tests[0].message).toContain('Command timed out');
-});
+  expect(result.status).toBe('fail')
+  expect(result.tests[0].name).toBe('Timeout Test')
+  expect(result.tests[0].status).toBe('fail')
+  expect(result.tests[0].message).toContain('Command timed out')
+})
 
 test('test passes when command completes before timeout', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Timeout Success Test',
-        'INPUT_COMMAND': 'sleep 1',
-        'INPUT_TIMEOUT': '5' //minutes
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Timeout Success Test',
+    INPUT_COMMAND: 'sleep 1',
+    INPUT_TIMEOUT: '5', // minutes
+  })
 
-    expect(result.status).toBe('pass');
-    expect(result.tests[0].name).toBe('Timeout Success Test');
-    expect(result.tests[0].status).toBe('pass');
-});
+  expect(result.status).toBe('pass')
+  expect(result.tests[0].name).toBe('Timeout Success Test')
+  expect(result.tests[0].status).toBe('pass')
+})
 
 test('test fails on setup command timeout', () => {
-    const result = runTestWithEnv({
-        'INPUT_TEST-NAME': 'Setup Timeout Test',
-        'INPUT_SETUP-COMMAND': 'sleep 3',
-        'INPUT_COMMAND': 'echo Hello, World!',
-        'INPUT_TIMEOUT': '0.01' // ~ 1 second
-    });
+  const result = runTestWithEnv({
+    'INPUT_TEST-NAME': 'Setup Timeout Test',
+    'INPUT_SETUP-COMMAND': 'sleep 3',
+    INPUT_COMMAND: 'echo Hello, World!',
+    INPUT_TIMEOUT: '0.01', // ~ 1 second
+  })
 
-    expect(result.status).toBe('fail');
-    expect(result.tests[0].name).toBe('Setup Timeout Test');
-    expect(result.tests[0].status).toBe('fail');
-    expect(result.tests[0].message).toContain('Command timed out');
-});
+  expect(result.status).toBe('fail')
+  expect(result.tests[0].name).toBe('Setup Timeout Test')
+  expect(result.tests[0].status).toBe('fail')
+  expect(result.tests[0].message).toContain('Command timed out')
+})
--- src/main.js
+++ src/main.js
@@ -1,72 +1,80 @@
-const core = require('@actions/core');
-const { execSync } = require('child_process');
+const core = require('@actions/core')
+const {execSync} = require('child_process')
+
+const env = {
+  PATH: process.env.PATH,
+  FORCE_COLOR: 'true',
+  DOTNET_CLI_HOME: '/tmp',
+  DOTNET_NOLOGO: 'true',
+  HOME: process.env.HOME,
+}
 
 function btoa(str) {
-    return Buffer.from(str).toString('base64');
+  return Buffer.from(str).toString('base64')
 }
 
 function generateResult(status, testName, command, message, duration, maxScore) {
-    return {
-        version: 1,
-        status: status,
-        max_score: maxScore,
-        tests: [{
-            name: testName,
-            status: status,
-            score: status === 'pass' ? maxScore : 0,
-            message: message,
-            test_code: command,
-            filename: "",
-            line_no: 0,
-            duration: duration
-        }]
-    };
+  return {
+    version: 1,
+    status,
+    max_score: maxScore,
+    tests: [
+      {
+        name: testName,
+        status,
+        score: status === 'pass' ? maxScore : 0,
+        message,
+        test_code: command,
+        filename: '',
+        line_no: 0,
+        duration,
+      },
+    ],
+  }
 }
 
 function getErrorMessage(error, command) {
-    if (error.message.includes("ETIMEDOUT")) {
-        return "Command timed out";
-    }
-    if (error.message.includes("command not found")) {
-        return `Unable to locate executable file: ${command}`;
-    }
-    if (error.message.includes("Command failed")) {
-        return "failed with exit code 1";
-    }
-    return error.message;
+  if (error.message.includes('ETIMEDOUT')) {
+    return 'Command timed out'
+  }
+  if (error.message.includes('command not found')) {
+    return `Unable to locate executable file: ${command}`
+  }
+  if (error.message.includes('Command failed')) {
+    return 'failed with exit code 1'
+  }
+  return error.message
 }
 
 function run() {
-    const testName = core.getInput('test-name', { required: true });
-    const setupCommand = core.getInput('setup-command');
-    const command = core.getInput('command', { required: true });
-    const timeout = parseFloat(core.getInput('timeout') || 10) * 60000; // Convert to minutes
-    const maxScore = parseInt(core.getInput('max-score') || 0);
+  const testName = core.getInput('test-name', {required: true})
+  const setupCommand = core.getInput('setup-command')
+  const command = core.getInput('command', {required: true})
+  const timeout = parseFloat(core.getInput('timeout') || 10) * 60000 // Convert to minutes
+  const maxScore = parseInt(core.getInput('max-score') || 0)
 
-    let output = '';
-    let startTime;
-    let endTime;
-    let result;
+  let output = ''
+  let startTime
+  let endTime
+  let result
 
-    try {
-        if (setupCommand) {
-            execSync(setupCommand, { timeout: timeout });
-        }
-
-        startTime = new Date();
-        output = execSync(command, { timeout: timeout }).toString();
-        endTime = new Date();
-
-        result = generateResult('pass', testName, command, output, endTime - startTime, maxScore);
-
-    } catch (error) {
-        endTime = new Date();
-        const errorMessage = getErrorMessage(error, command);
-        result = generateResult('fail', testName, command, errorMessage, endTime - startTime, maxScore);
+  try {
+    if (setupCommand) {
+      execSync(setupCommand, {timeout})
     }
 
-    core.setOutput('result', btoa(JSON.stringify(result)));
+    startTime = new Date()
+    output = execSync(command, {timeout, env}).toString()
+    endTime = new Date()
+
+    result = generateResult('pass', testName, command, output, endTime - startTime, maxScore)
+  } catch (error) {
+    endTime = new Date()
+    const errorMessage = getErrorMessage(error, command)
+    result = generateResult('fail', testName, command, errorMessage, endTime - startTime, maxScore)
+  }
+
+  core.setOutput('result', btoa(JSON.stringify(result)))
 }
 
-run();
-
+run()
